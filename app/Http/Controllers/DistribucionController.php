00ce880be593c5ec4810efdb8b9879819fb3135e
SebasBretones <47239648+SebasBretones@users.noreply.github.com>
2021-05-22 20:53:24 +0200

    ordenar mesas

--- app/Http/Controllers/DistribucionController.php
+++ app/Http/Controllers/DistribucionController.php
@@ -57,7 +57,7 @@ class DistribucionController extends Controller
      */
     public function show(Distribucion $distribucionmesa, Request $request)
     {
-        $mesas = Mesa::where('distribucion_id', $distribucionmesa->id)->orderBy('distribucion_id')
+        $mesas = Mesa::where('distribucion_id', $distribucionmesa->id)->orderBy('id')
         ->paginate(6)->withQueryString();
 
         return view('distribucionmesas.detalles', compact('distribucionmesa', 'mesas'));
