a50ab82cf6f8194cb4b75af1a42248ef64224c1b
SebasBretones <47239648+SebasBretones@users.noreply.github.com>
2021-05-28 14:47:10 +0200

    Custom error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('main')
+@section('content')
+<div class="row justify-content-center" id="loginc">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header text-center">Error 404</div>
+            <div class="card-body text-center">
+                {{ __('No hemos encontrado lo que estuvieras buscando') }}
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
