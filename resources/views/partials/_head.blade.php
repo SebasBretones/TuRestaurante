336651fd87269e6bf44e2d5a1a4ac659bab8c17c
SebasBretones <47239648+SebasBretones@users.noreply.github.com>
2021-06-06 19:31:14 +0200

    logo

--- resources/views/partials/_head.blade.php
+++ resources/views/partials/_head.blade.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <link rel="shortcut icon" type="image/png" href="{{ asset('img/fondo.jpg') }}">
-    <link rel="shortcut icon" sizes="192x192" href="{{ asset('img/fondo.jpg') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ asset('img/logo.jpg') }}">
+    <link rel="shortcut icon" sizes="192x192" href="{{ asset('img/logo.jpg') }}">
 
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link rel="preconnect" href="https://fonts.gstatic.com">
